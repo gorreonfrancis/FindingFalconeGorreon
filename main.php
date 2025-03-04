10b4fbbc09faa2ad07cc3aa1c5e7fc3927690af9
markfrancis <markfrancispgorreon@iskolarngbayan.pup.edu.ph>
2025-03-04 23:54:32 +0800

    Added main page

--- main.php
+++ main.php
@@ -1,1 +1,33 @@
-This is the main page
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Finding Falcone</title>
+    <link rel="stylesheet" href="css/main_page.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header class="header">
+        <img src="pics/logo.svg" alt="Finding Falcone Logo" class="logo">
+        <h3 class="header-title">Finding Falcone</h3>
+
+        <div class="right-section">
+            <div class="reset-container">
+                <img src="pics/reset_logo.svg" alt="Reset Logo" class="reset-logo">
+                <p class="reset">Reset</p>
+            </div>
+
+            <a href="homepage.php" class="home-container">
+                <img src="pics/home_logo.svg" alt="Home Logo" class="home-logo">
+                <p class="reset">Home</p>
+            </a>
+        </div>
+    </header>
+    <h1 class="select">Select planets you want to search in:</h1>
+</body>
+
+</html>
