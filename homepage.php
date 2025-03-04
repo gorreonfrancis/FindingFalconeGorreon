d04e74c1ef8e8a8ab2bfbb834ed094ea9941bfbe
markfrancis <markfrancispgorreon@iskolarngbayan.pup.edu.ph>
2025-03-04 18:13:56 +0800

    Added Homepage

---
+++ homepage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Finding Falcone</title>
+    <link rel="stylesheet" href="/css/homepage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="title-container">
+        <img src="pics/logo.svg" alt="Finding Falcone Logo" class="logo">
+        <h1 class="title">Finding Falcone</h1>
+    </div>
+
+    <p class="story">King Shan has received intelligence that Al Falcone is in hiding in one of these 6 planets - DonLon, Enchai, Jebing, Sapir, Lerbin & Pingasor. However he has limited resources at his disposal & can send his army to only 4 of these planets.</p>
+
+    <div class="button-container">
+        <button class="play-button">Play</button>
+    </div>
+
+</body>
+
+</html>
